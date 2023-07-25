b38f0254cf8fbf7ef00fecf7df60a0e299e6781e
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-07-25 12:48:52 +0100

    add func, struct.

--- main.h
+++ main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
+int _putchar(char c);
+int print_string(va_list s);
+int print_char(va_list c);
+int _printf(const char *format, ...);
+typedef struct code_format
+{
+  char *sc;
+  int (*f)(va_list);
+} code_f;
 #endif
