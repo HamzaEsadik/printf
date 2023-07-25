38ae8696d738e12f74cc58ab55a92387ae2c49e8
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-07-25 13:33:04 +0100

    edit decimal_print.c

--- decimal_print.c
+++ decimal_print.c
@@ -25,3 +25,24 @@ int print_dec(va_list d)
 			len++;
 		}
 		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 10;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			if (n < 0)
+				_putchar((digit * -1) + 48);
+			else
+				_putchar(digit + '0');
+			count++;
+			n -= digit * powten;
+			powten /= 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
