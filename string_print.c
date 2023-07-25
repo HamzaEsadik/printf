f8b6d0a2b5aa4816e38915b7620f9356c32acb70
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-25 11:59:35 +0000

    create string_print.c

---
+++ string_print.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_string - prints the string c to standard output
+ * @s: string printed
+ * Return: 1
+ */
+int print_string(va_list s)
+{
+	char *my_string;
+	int  j = 0;
+
+	my_string = va_arg(s, char *);
+	if (my_string == NULL)
+		my_string = "(null)";
+	while (my_string[j])
+	{
+		_putchar(my_string[j]);
+		j++;
+	}
+	return (j);
+}
