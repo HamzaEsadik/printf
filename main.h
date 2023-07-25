375ba341b6641059aa33627dc31be313132cbcaa
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-07-25 14:14:19 +0100

    comment main.h

--- main.h
+++ main.h
@@ -8,6 +8,11 @@ int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_int(va_list i);
 int print_dec(va_list d);
+/**
+  * struct code_format - stuct
+  * @sn: variable
+  * @f: The function associated
+  */
 typedef struct code_format
 {
 char *sn;
